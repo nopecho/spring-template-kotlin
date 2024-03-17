567db47398615c678f1eb368a45adc0f1278132a
nopecho <ch.jooon@gmail.com>
2024-03-17 14:17:43 +0900

    feat: add utils

---
+++ core/src/test/kotlin/TestUtils.kt
@@ -0,0 +1,25 @@
+import java.util.concurrent.CountDownLatch
+
+fun concurrencyCall(requestSize: Int = 100, task: () -> Unit): List<Throwable> {
+    val throwable = mutableListOf<Throwable>()
+    val startLatch = CountDownLatch(1)
+    val doneLatch = CountDownLatch(requestSize)
+
+    repeat(requestSize) {
+        Thread {
+            try {
+                startLatch.await()
+                task()
+            } catch (e: Throwable) {
+                throwable.add(e)
+            } finally {
+                doneLatch.countDown()
+            }
+        }.start()
+    }
+
+    Thread.sleep(500)
+    startLatch.countDown()
+    doneLatch.await()
+    return throwable
+}
